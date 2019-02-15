7ead963303f8aa4b2073e79dddc105d469ea5d87
Ashutosh Anand <ashutosh.anand13@gmail.com>
2019-02-15 20:46:29 +0530

    Commit deployment issue

--- src/main/java/com/winwithweb/application/WinWithWebConfigToolApplication.java
+++ src/main/java/com/winwithweb/application/WinWithWebConfigToolApplication.java
@@ -1,15 +1,20 @@
 package com.winwithweb.application;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WinWithWebConfigToolApplication {
+public class WinWithWebConfigToolApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(WinWithWebConfigToolApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(WinWithWebConfigToolApplication.class, args);
 	}
 
 }
-
