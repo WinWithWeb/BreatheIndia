8455f02b7ea07d17413954d385c4bb2401192963
sachingoyal <sachingoyal@192.168.1.103>
2019-02-17 20:42:00 +0530

    password encryption

---
+++ src/main/java/com/winwithweb/application/security/BcryptGenerator.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package com.winwithweb.application.security;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+/**
+ * @author sachingoyal
+ *
+ */
+public class BcryptGenerator {
+	
+
+//	public static void main(String[] args) {
+//
+//		int i = 0;
+//		while (i < 5) {
+//			String password = "password";
+//			BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+//			String hashedPassword = passwordEncoder.encode(password);
+//
+//			System.out.println(hashedPassword);
+//			i++;
+//		}
+//
+//	  }
+
+}
