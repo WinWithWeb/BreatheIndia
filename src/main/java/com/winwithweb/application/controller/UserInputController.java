e240b225975d19621156049c1884714686abd5b0
Ashutosh Anand <ashutosh.anand13@gmail.com>
2019-02-20 08:15:21 +0530

    Adding more input fields to Email Sender Form

--- src/main/java/com/winwithweb/application/controller/UserInputController.java
+++ src/main/java/com/winwithweb/application/controller/UserInputController.java
@@ -3,11 +3,14 @@ package com.winwithweb.application.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.winwithweb.application.model.EmailDetails;
 import com.winwithweb.application.model.User;
 import com.winwithweb.application.service.EmailUtility;
 
@@ -22,14 +25,15 @@ public class UserInputController {
 	
 	@RequestMapping(value = {"/","/home"}, method = RequestMethod.GET)
 	public String getHomePage(ModelMap model) {
-		User user = new User();
-		model.addAttribute("userdetails", user);
+		model.addAttribute("emaildata", new EmailDetails());
 		return "home";
 	}
 
 	@RequestMapping(value = "/home", method = RequestMethod.POST)
-	public String processUserform(@RequestParam String username, @RequestParam String email) {
-		LOGGER.info(username+"--"+email+"--");
+	public String processUserform(@ModelAttribute EmailDetails emaildata,Model model) {
+		emaildata.toString();
+		model.addAttribute("emaildata", new EmailDetails());
+		/*LOGGER.info(username+"--"+email+"--");
 		if(username=="" || email==""){
 			LOGGER.error("Username-"+username+"||"+email+"-");
 			return "home";
@@ -37,8 +41,8 @@ public class UserInputController {
 		else if(username.equals(null) || email.equals(null)){
 			LOGGER.error("Error");
 			return "home";
-		}
-		EmailUtility.sendEmail(email);
+		}*/
+		EmailUtility.sendEmail(emaildata.getRecepientemailIds());
 		return "home";
 	}
 
