42ac34887f046858bd383d791056ad003d293a6a
sachingoyal <sachingoyal@192.168.1.103>
2019-06-30 16:53:59 +0530

    Removed extra files

--- src/main/java/com/winwithweb/application/WinWithWebConfigToolApplication.java
+++ src/main/java/com/winwithweb/application/WinWithWebConfigToolApplication.java
@@ -2,16 +2,15 @@ package com.winwithweb.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class WinWithWebConfigToolApplication extends SpringBootServletInitializer {
 
-	@Override
+	/*	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(WinWithWebConfigToolApplication.class);
-	}
+	}*/
 
 	public static void main(String[] args) {
 		SpringApplication.run(WinWithWebConfigToolApplication.class, args);
