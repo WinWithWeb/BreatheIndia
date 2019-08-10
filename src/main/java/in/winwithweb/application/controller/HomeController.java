acf428ea4eef192a969d7035783aeaceae4e7543
sachingoyal <sachingoyal@192.168.43.246>
2019-08-10 15:13:47 +0530

    Add drop down

--- src/main/java/in/winwithweb/application/controller/HomeController.java
+++ src/main/java/in/winwithweb/application/controller/HomeController.java
@@ -1,12 +1,16 @@
 package in.winwithweb.application.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import in.winwithweb.application.model.Details;
 import in.winwithweb.application.model.Record;
 import in.winwithweb.application.model.Sample;
 import in.winwithweb.application.schedular.AirPollutionDataSchedular;
@@ -22,9 +26,31 @@ public class HomeController {
 			List<Record> record = data.getRecords();
 			if (record != null && !record.isEmpty()) {
 				model.addAttribute("recordList", record);
+				model.addAttribute("details", new Details());
+
+				if (AirPollutionDataSchedular.getStates() != null) {
+
+					model.addAttribute("stateList", AirPollutionDataSchedular.getStates());
+				} else {
+					model.addAttribute("stateList", new ArrayList<String>());
+
+				}
+
 			}
 		}
 		return "homeDemo";
+	}
+
+	@RequestMapping(value = "/getCitiesData")
+	@ResponseBody
+	public List<String> getCitiesData(@RequestParam String state) {
+		return AirPollutionDataSchedular.getCity(state);
+	}
+
+	@RequestMapping(value = "/getStationData")
+	@ResponseBody
+	public List<String> getStationData(@RequestParam String city) {
+		return AirPollutionDataSchedular.getStation(city);
 	}
 
 }
--- src/main/java/in/winwithweb/application/schedular/AirPollutionDataSchedular.java
+++ src/main/java/in/winwithweb/application/schedular/AirPollutionDataSchedular.java
@@ -5,7 +5,9 @@ package in.winwithweb.application.schedular;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
@@ -14,6 +16,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import in.winwithweb.application.model.Record;
 import in.winwithweb.application.model.Sample;
 
 /**
@@ -26,6 +29,13 @@ public class AirPollutionDataSchedular {
 	RestTemplate restTemplate;
 
 	static Sample data;
+
+	private static List<String> states = new ArrayList<String>();
+
+	private static Map<String, List<String>> region = new HashMap<String, List<String>>();
+	
+	private static Map<String, List<String>> stationMap = new HashMap<String, List<String>>();
+
 
 	private RestTemplate getRestTemplate() {
 		if (restTemplate == null) {
@@ -44,10 +54,56 @@ public class AirPollutionDataSchedular {
 		return data;
 	}
 
+	public static List<String> getStates() {
+		return states;
+	}
+
+	public static List<String> getCity(String stateName) {
+		return region.get(stateName);
+	}
+	
+	public static List<String> getStation(String city) {
+		return stationMap.get(city);
+	}
+
 	@Scheduled(cron = "0/10 * * * * ?")
 	public void cronJobSch() {
 		String url = "https://api.data.gov.in/resource/3b01bcb8-0b14-4abf-b6f2-c1bfd384ba69?api-key=579b464db66ec23bdd00000173db32215e6b4ad04b1c0d2d7138b31d&format=json&offset=0&limit=5000";
 		data = getRestTemplate().getForObject(url, Sample.class);
+		if (data != null) {
+			List<Record> recordList = data.getRecords();
+			states = new ArrayList<String>();
+			region = new HashMap<String, List<String>>();
+			for (Record record : recordList) {
+				if (!states.contains(record.getState())) {
+					states.add(record.getState());
+
+				}
+
+				if (region.containsKey(record.getState())) {
+					List<String> reg = region.get(record.getState());
+					if (!reg.contains(record.getCity())) {
+						reg.add(record.getCity());
+					}
+				} else {
+					List<String> cityList = new ArrayList<String>();
+					cityList.add(record.getCity());
+					region.put(record.getState(), cityList);
+				}
+				
+				if (stationMap.containsKey(record.getCity())) {
+					List<String> station = stationMap.get(record.getCity());
+					if (!station.contains(record.getStation())) {
+						station.add(record.getStation());
+					}
+				} else {
+					List<String> stationList = new ArrayList<String>();
+					stationList.add(record.getStation());
+					stationMap.put(record.getCity(), stationList);
+				}
+
+			}
+		}
 	}
 
 }
